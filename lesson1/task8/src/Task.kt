6b93d7171425f9d790c1400cc80d116ae49cc94b
Stefan Verhoeff <stefan.verhoeff@here.com>
2017-07-31 18:57:08 +0200

    First 8 solutions

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,7 +1,12 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
-){
-    TODO()
+) {
+    val personalInfo = client?.personalInfo
+
+    val email = personalInfo?.email ?: return
+    if (message == null) return
+
+    mailer.sendMessage(email, message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
